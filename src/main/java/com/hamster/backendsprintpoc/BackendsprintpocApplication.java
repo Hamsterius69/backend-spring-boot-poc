53eb8254cb157604ceb307ae86a34c813848c309
Ariel Aguilar <ariel@goja.com>
2022-09-11 15:47:27 -0400

    first commit

---
+++ src/main/java/com/hamster/backendsprintpoc/BackendsprintpocApplication.java
@@ -0,0 +1,13 @@
+package com.hamster.backendsprintpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendsprintpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendsprintpocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hamster/backendsprintpoc/BackendsprintpocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hamster.backendsprintpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendsprintpocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
